ac081155d1bcea706d7ba68b6bd6374715fa167c
Sosed_11 <kost1991ya@mail.ru>
2022-10-17 23:43:50 +0500

    Готово

--- src/Human.java
+++ src/Human.java
@@ -1,6 +1,21 @@
 
 
 public class Human {
+    int yearOfBirth;
+    String name;
+    String currentCity;
+    String job;
+
+    Human(int yearOfBirth, String name, String currentCity, String job){
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.currentCity = currentCity;
+        this.job = job;
+    }
+
+    void greetings() {
+        System.out.println("Привет! Меня зовут " + name + ". " + "Я из города " + currentCity + ". " +  "Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
 
     }
+}
 
